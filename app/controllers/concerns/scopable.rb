3a2f5bc2210e25e621a58b1444e94e518baf9b0f
Samuelfaure <samuel.faure.dev@gmail.com>
2019-07-08 11:34:10 +0200

    Add controllers etablissements unites_legales v3 with scope concern to filter on any fields

--- app/controllers/concerns/scopable.rb
+++ app/controllers/concerns/scopable.rb
@@ -2,6 +2,7 @@ module Scopable
   module Controller
     extend ActiveSupport::Concern
 
+<<<<<<< HEAD
     included do
       controller_attributes.each { |attr| add_has_scope_for_attribute(attr) }
     end
@@ -15,6 +16,12 @@ module Scopable
 
       def add_has_scope_for_attribute(attr)
         has_scope(attr.to_sym, ->(value) { where(Hash[attr, value]) }, only: :index)
+=======
+    # Add has_scope for each model attribute
+    included do
+      controller_name.classify.constantize.attribute_names.each do |a|
+        has_scope a.to_sym, ->(value) { where(Hash[a, value]) }, only: :index
+>>>>>>> Add controllers etablissements unites_legales v3 with scope concern to filter on any fields
       end
     end
   end
@@ -22,6 +29,7 @@ module Scopable
   module Model
     extend ActiveSupport::Concern
 
+<<<<<<< HEAD
     included do
       attribute_names.each { |attr| add_scope_for_attribute(attr) }
     end
@@ -31,6 +39,12 @@ module Scopable
 
       def add_scope_for_attribute(attr)
         scope attr.to_sym, ->(value) { where(Hash[attr, value]) }
+=======
+    # Add scope for each model attribute
+    included do
+      self.attribute_names.each do |a|
+        scope a.to_sym, ->(value) { where(Hash[a, value]) }
+>>>>>>> Add controllers etablissements unites_legales v3 with scope concern to filter on any fields
       end
     end
   end
